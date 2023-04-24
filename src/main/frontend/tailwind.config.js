ae07dc3dc3dda98f9e0ff3179196b5bf05ac13cc
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-24 07:18:31 -0500

    auto save script Mon Apr 24 07:18:31 CDT 2023

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["../resources/templates/**/*.{html,js}"], // it will be explained later
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        syne: ['"Syne"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
