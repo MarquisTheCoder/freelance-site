3ad73ef1079ed9c19d1f2444d8b3c462ce039dc8
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-24 05:38:49 -0500

    auto save script Mon Apr 24 05:38:49 CDT 2023

--- src/main/java/com/marquis/coder/CoderApplication.java
+++ src/main/java/com/marquis/coder/CoderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoderApplication {
+public class CoderApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoderApplication.class, args);
