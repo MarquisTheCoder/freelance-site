ef6930f7ce054957431b8fad976776cc8954f511
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-08-21 22:30:00 -0500

    auto save script Mon Aug 21 22:30:00 CDT 2023

--- src/main/resources/static/js/src/language.js
+++ src/main/resources/static/js/src/language.js
@@ -1,1 +1,5 @@
-(async () => {})();
+(async () => {
+  const languageDropdown = document.querySelector(".language-dropdown");
+  const languageList = document.querySelector(".language-list");
+  const languages = languageList.querySelectorAll("li");
+})();
