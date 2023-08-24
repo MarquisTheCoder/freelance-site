0f19b48532f38cb4fb052ad3e767578db4b63cae
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-08-24 12:53:10 -0500

    auto save script Thu Aug 24 12:53:10 CDT 2023

--- src/main/java/com/marquis/coder/Services/EmailService.java
+++ src/main/java/com/marquis/coder/Services/EmailService.java
@@ -1,19 +1,27 @@
 package com.marquis.coder.Services;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender; 
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmailService {
-   JavaMailSender mailSender;
-   public EmailService(JavaMailSender mailSender) {
-    this.mailSender = mailSender;
-   }
+import lombok.RequiredArgsConstructor;
 
-   void sendMessage(String to, String subject, String text){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("EMAIL");
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+
+   @Autowired 
+   private final JavaMailSender javaMailSender;
+
+   void sendEmail(String to, String subject, String text){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(to);
+        mailMessage.setSubject(subject);
+        mailMessage.setText(text);
+        mailMessage.setFrom("deshawn.m.williams01@gmail.com");
+        javaMailSender.send(mailMessage);
    }
 }
